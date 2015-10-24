ccd4f210dbea4319a29b8e0fb4c509849cef6369
Sam Fisher <sam@circuit-diagram.org>
2015-10-24 13:00:18 +0100

    Build script

---
+++ build.ps1
@@ -0,0 +1,6 @@
+# Generate PNG icons from SVGs
+dir -recurse Icons/**/*.svg | Foreach { ./convert $_.FullName -resize 32x32 -density 96x96 "PNGIcons/$($_.BaseName)_32.png" }
+dir -recurse Icons/**/*.svg | Foreach { ./convert -density 192 $_.FullName "PNGIcons/$($_.BaseName)_64.png" }
+
+# Build XML to CDCOM
+./cdcompile.exe -i Components -o Build --icondir PNGIcons -r -v -c -s
