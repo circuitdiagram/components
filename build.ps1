30aac99e4700bf0ef75e51d29705ec008a05156e
Sam Fisher <sam@circuit-diagram.org>
2015-11-15 12:16:29 +0000

    Output manifest

--- build.ps1
+++ build.ps1
@@ -3,6 +3,6 @@ dir -recurse Icons/**/*.svg | Foreach { ./convert $_.FullName -resize 32x32 -den
 dir -recurse Icons/**/*.svg | Foreach { ./convert -density 192 $_.FullName "PNGIcons/$($_.BaseName)_64.png" }
 
 # Build XML to CDCOM
-./cdcompile.exe -i Components -o Build --icondir PNGIcons -r -v -c -s
+./cdcompile.exe -i Components -o Build --icondir PNGIcons -r -v --manifest Build/manifest.xml
 
 exit $LASTEXITCODE
