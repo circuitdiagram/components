ff0936500e3887642c0b12cd905a5075a06a6f68
Sam Fisher <sam@circuit-diagram.org>
2018-10-03 20:46:04 +0100

    Remove build file

--- build.ps1
+++
@@ -1,8 +0,0 @@
-# Generate PNG icons from SVGs
-dir -recurse Icons/**/*.svg | Foreach { ./convert $_.FullName -resize 32x32 -density 96x96 "PNGIcons/$($_.BaseName)_32.png" }
-dir -recurse Icons/**/*.svg | Foreach { ./convert -density 192 $_.FullName "PNGIcons/$($_.BaseName)_64.png" }
-
-# Build XML to CDCOM
-dotnet $env:cdcompile Components --recursive --cdcom Build --resources PNGIcons --svg Preview --manifest Build/manifest.xml
-
-exit $LASTEXITCODE
